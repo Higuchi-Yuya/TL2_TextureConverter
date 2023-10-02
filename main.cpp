6011c803c2ac54ebdac8592d144496b3f3e7ece0
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2023-10-02 15:23:36 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+
+	// argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (size_t i = 0; i < argc; i++)
+	{
+		// •¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		// ‰üs
+		printf("\n");
+	}
+	
+	system("pause");
+
+	return 0;
+}
